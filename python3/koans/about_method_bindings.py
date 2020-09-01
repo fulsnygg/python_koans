0f33c5b564e4be1b7597c5d14462f9f6c4733941
fulsnygg <nils.fredrikson@gmail.com>
2020-09-01 16:30:46 +0200

    method bindings lesson

--- python3/koans/about_method_bindings.py
+++ python3/koans/about_method_bindings.py
@@ -16,43 +16,50 @@ class Class:
 class AboutMethodBindings(Koan):
     def test_methods_are_bound_to_an_object(self):
         obj = Class()
-        self.assertEqual(__, obj.method.__self__ == obj)
+        self.assertEqual(True, obj.method.__self__ == obj)
 
     def test_methods_are_also_bound_to_a_function(self):
         obj = Class()
-        self.assertEqual(__, obj.method())
-        self.assertEqual(__, obj.method.__func__(obj))
+        self.assertEqual("parrot", obj.method())
+        self.assertEqual("parrot", obj.method.__func__(obj))
 
     def test_functions_have_attributes(self):
         obj = Class()
-        self.assertEqual(__, len(dir(function)))
-        self.assertEqual(__, dir(function) == dir(obj.method.__func__))
+        self.assertEqual(35, len(dir(function)))
+        self.assertEqual(True, dir(function) == dir(obj.method.__func__))
 
     def test_methods_have_different_attributes(self):
         obj = Class()
-        self.assertEqual(__, len(dir(obj.method)))
+        self.assertEqual(27, len(dir(obj.method)))
+        # Methods are basically functions associated with objects/classes
 
     def test_setting_attributes_on_an_unbound_function(self):
         function.cherries = 3
-        self.assertEqual(__, function.cherries)
+        function.fulsnygg = 7612387612638
+        self.assertEqual(3, function.cherries)
+        self.assertEqual(7612387612638, function.fulsnygg)
 
     def test_setting_attributes_on_a_bound_method_directly(self):
         obj = Class()
-        with self.assertRaises(___): obj.method.cherries = 3
+        with self.assertRaises(AttributeError): obj.method.cherries = 3
 
     def test_setting_attributes_on_methods_by_accessing_the_inner_function(self):
         obj = Class()
         obj.method.__func__.cherries = 3
-        self.assertEqual(__, obj.method.cherries)
+        self.assertEqual(3, obj.method.cherries)
 
     def test_functions_can_have_inner_functions(self):
         function2.get_fruit = function
-        self.assertEqual(__, function2.get_fruit())
+        self.assertEqual("pineapple", function2.get_fruit())
 
     def test_inner_functions_are_unbound(self):
         function2.get_fruit = function
-        with self.assertRaises(___): cls = function2.get_fruit.__self__
+        with self.assertRaises(AttributeError): cls = function2.get_fruit.__self__
 
+    def test_is_inner_function_in_methods_also_unbound(self):
+        obj = Class()
+        with self.assertRaises(AttributeError): cls = obj.method.__func__.__self__
+        # Yes!
     # ------------------------------------------------------------------
 
     class BoundClass:
@@ -68,9 +75,10 @@ class AboutMethodBindings(Koan):
         #   binding_owner = obj
         #   owner_type = cls
 
-        self.assertEqual(__, bound_obj.__class__.__name__)
-        self.assertEqual(__, binding_owner.__class__.__name__)
+        self.assertEqual("BoundClass", bound_obj.__class__.__name__)
+        self.assertEqual("AboutMethodBindings", binding_owner.__class__.__name__)
         self.assertEqual(AboutMethodBindings, owner_type)
+        self.assertEqual(AboutMethodBindings, binding_owner.__class__)
 
     # ------------------------------------------------------------------
 
@@ -86,5 +94,20 @@ class AboutMethodBindings(Koan):
     def test_set_descriptor_changes_behavior_of_attribute_assignment(self):
         self.assertEqual(None, self.color.choice)
         self.color = 'purple'
-        self.assertEqual(__, self.color.choice)
+        self.assertEqual('purple', self.color.choice)
 
+    class SuperDuperColor:
+        def __init__(self):
+            self.choice = None
+
+    duper_color = SuperDuperColor()
+
+    def test_without_set_descriptor(self):
+        self.assertEqual(None, self.duper_color.choice)
+        self.duper_color = 'red'
+        try:
+             'red' == self.duper_color.choice
+        except AttributeError as ex:
+            err_msg = ex.args[0]
+
+        self.assertRegex("'str' object has no attribute 'choice'", err_msg)
